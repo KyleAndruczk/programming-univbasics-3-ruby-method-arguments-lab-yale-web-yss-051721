8a6a4c2ebcfe4d9f7ef382c10f265cbbd073df37
thoughtful-gateway-b1af <kyle.andruczk@gmail.com>
2021-05-20 21:52:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,9 +4,19 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
+def introduction(name)
+  
+  "Hi, my name is #{name}."
+  
+end 
+
+def introduction_with_language(name, language)
 
 
-
+  
+  
+  
+end 
 
 
 
